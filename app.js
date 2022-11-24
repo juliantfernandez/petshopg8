8fa045110b6673df30b306f1c4cc890fbaab37f9
juliantfernandez <juliant.fernandez0611@gmail.com>
2022-11-24 18:25:20 -0300

    testing deploy

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ let express = require('express')
 let app = express()
 
 
-app.listen(3000)
+app.listen(3000, ()=>{
+    console.log('Server listo');
+})
 
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Hola Mundo')
 })
