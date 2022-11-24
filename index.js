aef644b8a68dde80a5ad3c30b64a7250271ae95a
juliantfernandez <juliant.fernandez0611@gmail.com>
2022-11-24 18:55:08 -0300

    test deploy

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 let express = require('express')
-
+const path = require('path')
 let app = express()
+const publicPath = path.resolve(__dirname, './public')
+app.set('view engine', 'ejs')
+app.use(express.static(publicPath))
+app.use(express.urlencoded({extended:false})); //Captura info x POST
+app.use(express.json()); // POST
 
+const methodOverride = require('method-override'); // PUT,DELETE
+app.use(methodOverride('_method')); // PUT,DELETE
+
+let productsRoutes = require('./src/routes/productsRoutes')
+app.use('/', productsRoutes)
+
+// const usersRoutes = require('./src/routes/usersRoutes')
+// app.use('/', usersRoutes)
+
+// const petshopRoutes = require('./src/routes/petshopRoutes')
+// app.use('/', petshopRoutes)
 
 app.listen(3000 || process.env.PORT, ()=>{
     console.log('Server listo');
 })
 
-app.get('/', (req, res) => {
-    res.send('Hola Mundo')
-})
---
+++ src/routes/productsRoutes.js
@@ -0,0 +1,27 @@
+let express = require('express');
+const router = express.Router();
+let productsController = require('../controllers/productsController.js');
+
+
+
+router.get('/', productsController.index)
+
+router.get('/productCreate',productsController.create)
+
+router.get('/productEdit/:id', productsController.edit)
+
+router.put('/productEdit/:id', productsController.update)
+
+router.get('/productDetail', productsController.detail)
+
+router.get('/productDetail/:id', productsController.detailId)
+
+router.post('/productCreate' ,productsController.store)
+
+router.delete('/delete/:id', productsController.delete)
+
+
+
+
+
+module.exports = router
